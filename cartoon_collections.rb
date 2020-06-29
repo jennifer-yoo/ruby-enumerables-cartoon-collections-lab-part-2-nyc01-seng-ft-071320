b326887144a668e19268489f1790060b2a7d68a5
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-29 05:01:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,6 @@ end
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |string| string.length > 4 }
 end
-  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
-  # Return the boolean result of this check
-end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
