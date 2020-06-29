2b3bd572bddeaf9fee30dcccdf3559721e60aea8
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-29 05:16:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,8 @@ def find_valid_calls(planeteer_calls)
   count = 0
 
   while count < valid_calls.length
-
   planeteer_calls.find { |element|  element == valid_calls[count] }
-
-    end
-    count += 1
+  puts element
+  end
+  count += 1
 end
